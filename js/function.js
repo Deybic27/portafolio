58023bdf84559e991c8d9075fd69d410608e3a29
Deybic Rojas <deybic9715@gmail.com>
2021-12-14 17:08:37 -0500

    Agregue seccion proyectos, experiecncia, blogpost

--- js/function.js
+++ js/function.js
@@ -22,11 +22,35 @@ $(document).ready(function() {
     $('span.content-rotating-text').text(arrayVal[0]);
     setInterval(()=>{
         rotatingText(arrayVal)
-        }, 3000);
+    }, 3000);
+    
+    var hide = 4;
+    var hidep = 3;
 
-    /*$('a').click(function(event) {
+    $("a#next").click(function(event) {
         event.preventDefault();
-        $(this).hide("slow");
-    });*/
+        var show = hide-3;
+        console.log(hide+' '+show);
+        if ($("#post"+hide).is(':hidden')) {
+            $("#post"+show).hide("slow");
+            $("#post"+hide).show(2000);
+            hidep=hide-hidep;
+            hide+=1;
+            console.log(hidep);
+        }else{
+            hide-=3;
+        }
+    });
+
+    $("a#previous").click(function(event) {
+        event.preventDefault();
+        var show = hidep-3;
+        console.log(hidep+' '+show);
+        if ($("#post"+hidep).is(':hidden')) {
+            $("#post"+show).hide("slow");
+            $("#post"+hidep).show(2000);
+            hidep+=1;
+        }
+    });
 });
 
